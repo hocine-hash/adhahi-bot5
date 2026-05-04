93034603e8636f64d2459b2b9be7980d0c03ab16
hocine-hash <boussalmhocine@gmail.com>
2026-05-04 13:49:35 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,20 @@
 import requests
 import os
 
+URL = "https://adhahi.dz"
+
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-requests.post(url, data={"chat_id": CHAT_ID, "text": "🔥 البوت يعمل بنجاح"})
+def send_msg(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
+
+def check():
+    r = requests.get(URL, timeout=10)
+
+    # شرط سوق أهراس
+    if "سوق أهراس" in r.text and "غير متوفر" not in r.text:
+        send_msg("🚨 الحجز فتح في سوق أهراس!\nhttps://adhahi.dz")
+
+check()
