b95c72912b06e0a5c5d9a3f513081bbebb8a9c09
hocine-hash <boussalmhocine@gmail.com>
2026-05-04 14:22:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 import requests
 
-# ضع توكن بوتك هنا (من BotFather)
-TOKEN = "8530154647:AAESvKZjbvg_zU6FMZ8DIROkIMCv-8KdwEQ"
+URL = "https://adhahi.dz"
 
-# هذا هو Chat ID الخاص بك
+TOKEN = "8530154647:AAESvKZjbvg_zU6FMZ8DIROkIMCv-8KdwEQ"
 CHAT_ID = "7498031156"
 
 def send_msg(text):
@@ -11,6 +10,10 @@ def send_msg(text):
     requests.post(url, data={"chat_id": CHAT_ID, "text": text})
 
 def check():
-    send_msg("🔥 البوت يعمل بنجاح")
+    r = requests.get(URL, timeout=10)
+
+    # شرط سوق أهراس
+    if "سوق أهراس" in r.text and "غير متوفر" not in r.text:
+        send_msg("🚨 الحجز فتح في سوق أهراس!\nhttps://adhahi.dz")
 
 check()
