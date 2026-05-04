2eff18bd0671159da50bbc6f968673c21fb8a291
hocine-hash <boussalmhocine@gmail.com>
2026-05-04 13:26:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,7 @@
-import os
 import requests
 
-URL = "https://adhahi.dz"
+TOKEN = "حط_توكن_هنا"
+CHAT_ID = "حط_الرقم_هنا"
 
-TOKEN = os.getenv("TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-def send_msg(text):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
-
-def check():
-    try:
-        send_msg("✅ البوت يعمل بشكل صحيح")  # اختبار
-
-        r = requests.get(URL, timeout=10)
-
-        if "سوق أهراس" in r.text and "غير متوفر" not in r.text:
-            send_msg("🚨 الحجز فتح في سوق أهراس!\nhttps://adhahi.dz")
-
-    except Exception as e:
-        print(e)
-
-check()
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+requests.post(url, data={"chat_id": CHAT_ID, "text": "اختبار فقط"})
